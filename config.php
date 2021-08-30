bff88744a8a5440ca309698039a5c56b274c9998
TradeHost <88116892+TradeHost@users.noreply.github.com>
2021-08-30 10:02:27 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,15 +1,15 @@
 <?php
-$servername = "localhost";
-$username = "";
-$password = "";
-$dbname = "";
+$servername = "213.136.76.119";
+$username = "tradehos_zyrotest";
+$password = "fBGXs90l]rV6*1";
+$dbname = "tradehos_zyrotest";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Site domain
 $site_domain = "limitednodes.host";
 
 // Pterodactyl API settings
-$ptero_domain = "";
+$ptero_domain = "gp.trade-mc.tk";
 $ptero_key = "";
 
 // Payment settings
@@ -18,10 +18,10 @@ $paypal['email'] = "";
 // Discord server settings
 $discord['autojoin_role'] = "C582892108046663690"; //role ID
 $discord['autojoin_guildid'] = "579239670639099915"; //server ID
-$discord['bot_token'] = "";
+$discord['bot_token'] = "ODY1OTI1NzI5MDAyNDU1MDQy.YPLGLQ.w7cQyQ4Fb_lFk7SphWb1fbhBhbU";
 
 // Discord OAUTH2 Settings
-$discord_oauth2['client_id'] = "";
-$discord_oauth2['client_secret'] = "";
-$discord_oauth2['redirect_uri'] = "http://domain.host/login";
+$discord_oauth2['client_id'] = "865925729002455042";
+$discord_oauth2['client_secret'] = "ogSc6-E_lztVTkxr9TJDIWiGBGJyyAwY";
+$discord_oauth2['redirect_uri'] = "https://falixnodesv1.herokuapp.comt/login";
 ?>
